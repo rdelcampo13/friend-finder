ad3942cb1a6fe87305dfc4bada4360ca53886649
Rudy Del Campo <r.delcampo13@gmail.com>
2017-10-29 20:35:43 -0500

    update final html

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,8 +2,8 @@
 // =============================================================
 var users = [
   {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Derek",
+    "photo": "https://static.pexels.com/photos/428339/pexels-photo-428339.jpeg",
     "scores[]": [
     "5",
     "1",
@@ -18,8 +18,8 @@ var users = [
     ]
   },
   {
-    "name": "Jacob Deming",
-    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "name": "Veronica",
+    "photo": "https://static.pexels.com/photos/415829/pexels-photo-415829.jpeg",
     "scores[]": [
     "4",
     "2",
@@ -34,85 +34,21 @@ var users = [
     ]
   },
   {
-    "name": "Jeremiah Scanlon",
-    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
-    "scores[]": [
-    "5",
-    "2",
-    "2",
-    "2",
-    "4",
-    "1",
-    "3",
-    "2",
-    "5",
-    "5"
-    ]
-  },
-  {
-    "name": "Louis T. Delia",
-    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-    "scores[]": [
-    "3",
-    "3",
-    "4",
-    "2",
-    "2",
-    "1",
-    "3",
-    "2",
-    "2",
-    "3"
-    ]
-  },
-  {
-    "name": "Lou Ritter",
-    "photo": "https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+    "name": "Rudy",
+    "photo": "https://static.pexels.com/photos/220453/pexels-photo-220453.jpeg",
     "scores[]": [
     "4",
-    "3",
-    "4",
-    "1",
-    "5",
     "2",
     "5",
-    "3",
     "1",
-    "4"
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
     ]
-  },
-  {
-    "name": "Jordan Biason",
-    "photo": "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
-    "scores[]": [
-    "4",
-    "4",
-    "2",
-    "3",
-    "2",
-    "2",
-    "3",
-    "2",
-    "4",
-    "5"
-    ]
-  },
-  {
-    "name": "Grimace",
-    "photo": "http://vignette2.wikia.nocookie.net/ronaldmcdonald/images/a/a9/Grimace.jpg",
-    "scores[]": [
-    "5",
-    "1",
-    "1",
-    "1",
-    "5",
-    "1",
-    "1",
-    "1",
-    "5",
-    "5"
-    ]
-  },
+  }
 ];
 
 module.exports = {
