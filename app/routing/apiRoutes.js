097c02080d7984d76a31f014149b10c7b5a70de0
Rudy Del Campo <r.delcampo13@gmail.com>
2017-11-01 01:11:11 -0500

    final bug fixes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,7 @@ var Friend = require(path.join(__dirname, "../data/Friend.js"));
 module.exports.setRoute = function(app) {
 
   // Sets up the Express app to handle data parsing
-  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
 
@@ -25,7 +25,7 @@ module.exports.setRoute = function(app) {
   app.post("/api/friends", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
-    var newFriend = new Friend(req.body.name, req.body.photo, req.body['scores[]']);
+    var newFriend = new Friend(req.body.name, req.body.photo, req.body.scores);
     var compatibleFriend = newFriend.runCompTest(friends);
 
     friends.push(newFriend);
